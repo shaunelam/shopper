06c0d305803270b4797419e97969c5dd7a1633ae
shaunelam <58092676+shaunelam@users.noreply.github.com>
2019-12-05 11:12:23 -0800

    Updates

---
+++ precache-manifest.9f8c566fab81980986a0bdaf8d19963e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9966558d4b7a890a7c24495dd1475b4",
+    "url": "/shopper/index.html"
+  },
+  {
+    "revision": "09d354c9f8f202e818db",
+    "url": "/shopper/static/css/main.9625c904.chunk.css"
+  },
+  {
+    "revision": "d4430cd3b0ee086eabae",
+    "url": "/shopper/static/js/2.75b3089a.chunk.js"
+  },
+  {
+    "revision": "09d354c9f8f202e818db",
+    "url": "/shopper/static/js/main.4c88e6de.chunk.js"
+  },
+  {
+    "revision": "40ab8780780289df2e81",
+    "url": "/shopper/static/js/runtime-main.d9159bab.js"
+  }
+]);
